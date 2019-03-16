9392f6cf9b2c19919b663e8ac04aad68ddf974a9
litongzhe <297025046@qq.com>
2019-03-16 18:05:40 +0800

    将全国省市区信息导入到数据库中并生成json文件

--- src/main/java/com/raising/modules/buildingPrice/controller/CountrypcrController.java
+++ src/main/java/com/raising/modules/buildingPrice/controller/CountrypcrController.java
@@ -1,11 +1,15 @@
 package com.raising.modules.buildingPrice.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
 import com.raising.modules.buildingPrice.entity.cityEntity;
 import com.raising.modules.buildingPrice.entity.provinceEntity;
 import com.raising.modules.buildingPrice.entity.regionEntity;
 import com.raising.modules.buildingPrice.service.InfodataService;
 import com.raising.modules.buildingPrice.service.PricehistorynewService;
 import com.raising.modules.buildingPrice.service.RegioninfoService;
+import net.sf.json.JSONArray;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +21,11 @@ import com.raising.framework.mybaits.Page;
 import com.raising.modules.buildingPrice.entity.CountrypcrEntity;
 import com.raising.modules.buildingPrice.service.CountrypcrService;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *  控制器
@@ -121,16 +128,15 @@ public class CountrypcrController extends BaseController {
         return countrypcrService.delete(id);
     }
     /**
-     * 全国省市区名
+     * 将全国省市区信息导入到数据库中
      *
      * @return ResultVo
      * @author litongzhe
-     * @datetime 2019年3月15日15点11分
+     * @datetime 2019年3月16日18点04分
      */
     @GetMapping("/PCRInfo")
     @PostMapping("/PCRInfo")
     public void PCRInfo() {
-        provinceEntity provincePCR = new provinceEntity();
         List<provinceEntity> proentities = (List<provinceEntity>)pricehistorynewService.getProvince().getData();
 
         List<String> provinces = new ArrayList<>();
@@ -160,4 +166,85 @@ public class CountrypcrController extends BaseController {
             }
         }
     }
+
+    /**
+     * 生成全国省市区的json文件
+     *
+     * @return ResultVo
+     * @author litongzhe
+     * @datetime 2019年3月16日18点02分
+     */
+    @GetMapping("/countryPCR")
+    public void countryPCR(){
+        List<provinceEntity> proentities = (List<provinceEntity>)pricehistorynewService.getProvince().getData();
+
+        List<String> provinces = new ArrayList<>();
+        for(provinceEntity e:proentities){
+            provinces.add(e.getProvince());
+        }
+        cityEntity cityPCR = new cityEntity();
+        regionEntity regionPCR = new regionEntity();
+        List<Map> countryInfo = new ArrayList<>();
+        for(String p:provinces){
+            Map<String,Object> provinceInfo= Maps.newLinkedHashMap();
+            provinceInfo.put("value",p);
+            provinceInfo.put("label",p);
+            cityPCR.setProvince(p);
+            List<cityEntity> cityentities = (List<cityEntity>)pricehistorynewService.getCity(cityPCR).getData();
+
+            List<Map> cities = new ArrayList<>();
+            for(cityEntity c:cityentities){
+                Map<String,Object> cityInfo = Maps.newLinkedHashMap();
+                String city = c.getCity();
+                cityInfo.put("value",city);
+                cityInfo.put("label",city);
+                regionPCR.setProvince(p);
+                regionPCR.setCity(city);
+                List<regionEntity> regionentities = (List<regionEntity>)pricehistorynewService.getRegion(regionPCR).getData();
+
+                List<Map> regions = new ArrayList<>();
+                for(regionEntity r:regionentities){
+                    String region = r.getCitylevel();
+                    if(!region.equals("无")){
+                        Map<String,Object> regionInfo = Maps.newLinkedHashMap();
+                        regionInfo.put("value",region);
+                        regionInfo.put("label",region);
+                        regions.add(regionInfo);
+                    }
+                }
+                cityInfo.put("children",regions);
+                cities.add(cityInfo);
+            }
+            provinceInfo.put("children",cities);
+            countryInfo.add(provinceInfo);
+        }
+        String json = JSON.toJSONString(countryInfo);
+        Gson gson = new Gson();
+        String str = gson.toJson(countryInfo);
+        BufferedWriter writer = null;
+        File file = new File("d:\\countryPCR.json");
+        if(!file.exists()){
+            try{
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try{
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file,false),"UTF-8"));
+            writer.write(str);
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if(writer!= null){
+                    writer.close();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
