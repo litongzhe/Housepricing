43c9b1a7c73af5bc24808870fea8b86af64e995e
conghongyu <c_87564512@sina.com>
2019-03-22 19:28:59 +0800

    楼盘详情图片接口联通

--- src/main/java/com/raising/modules/buildingPrice/entity/QueryInfoData.java
+++ src/main/java/com/raising/modules/buildingPrice/entity/QueryInfoData.java
@@ -3,61 +3,44 @@ package com.raising.modules.buildingPrice.entity;
 import java.io.Serializable;
 
 public class QueryInfoData extends InfodataEntity implements Serializable {
-    String startPrice;
-    String endPrice;
-
-    @Override
-    public String toString() {
-        return "QueryInfoData{" +
-                "startPrice='" + startPrice + '\'' +
-                ", endPrice='" + endPrice + '\'' +
-                ", startArea='" + startArea + '\'' +
-                ", endArea='" + endArea + '\'' +
-                '}';
-    }
-
-    public String getStartPrice() {
-        return startPrice;
-    }
-
-    public void setStartPrice(String startPrice) {
-        this.startPrice = startPrice;
-    }
-
-    public String getEndPrice() {
-        return endPrice;
-    }
-
-    public void setEndPrice(String endPrice) {
-        this.endPrice = endPrice;
-    }
-
-    public String getStartArea() {
-        return startArea;
-    }
-
-    public void setStartArea(String startArea) {
-        this.startArea = startArea;
-    }
-
-    public String getEndArea() {
-        return endArea;
-    }
-
-    public void setEndArea(String endArea) {
-        this.endArea = endArea;
-    }
+    int startPrice;
+    int endPrice;
+    int startArea;
 
     public QueryInfoData() {
     }
 
-    public QueryInfoData(String startPrice, String endPrice, String startArea, String endArea) {
+    public int getStartPrice() {
+        return startPrice;
+    }
+
+    public void setStartPrice(int startPrice) {
         this.startPrice = startPrice;
+    }
+
+    public int getEndPrice() {
+        return endPrice;
+    }
+
+    public void setEndPrice(int endPrice) {
         this.endPrice = endPrice;
+    }
+
+    public int getStartArea() {
+        return startArea;
+    }
+
+    public void setStartArea(int startArea) {
         this.startArea = startArea;
+    }
+
+    public int getEndArea() {
+        return endArea;
+    }
+
+    public void setEndArea(int endArea) {
         this.endArea = endArea;
     }
 
-    String startArea;
-    String endArea;
+    int endArea;
 }
